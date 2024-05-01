28c925a93c65b82ec658109c18e036670a7eeb3a
Steph <Steph@Olivers-iMac.local>
2024-05-01 13:00:04 +1000

    first draft

---
+++ main.py
@@ -0,0 +1,168 @@
+# Dictionary of food combinations
+food_combinations = {
+
+    ('any mushrooms'): 'Mushroom Skewer',
+    ('any meat', 'any mushrooms'): 'Meat and Mushroom Skewer',
+    ('any fish', 'any mushrooms'): 'Fish and Mushroom Skewer',
+    ('any fish', 'any mushrooms'): 'Fish and Mushroom Skewer',
+    ('any fruit', 'any plant'): 'Steamed Fruit',
+    ('hylian tomatoes', 'any plant'): 'Steamed Tomatoes',
+    ('any mushrooms', 'any plant'): 'Steamed Mushrooms',
+    ('any meat', 'any plant'): 'Steamed Meat',
+    ('any fish', 'any plant'): 'Steamed Fish',
+    ('spicy peppers'): 'Spicy Sauteed Peppers',
+    ('any nut'): 'Sauteed Nuts',
+    ('any plant'): 'Fried Wild Greens', 
+    ('stambulb'): 'Cooked Stambulb',
+    ('stambulb','goat butter'): 'Buttered Stambulb',
+    ('raw meat','goron spice'): 'Spiced Meat Skewer',
+    ('raw prime meat','goron spice'): 'Prime Spiced Meat Skewer',
+    ('raw gourmet meat','goron spice'): 'Gourmet Spiced Meat Skewer',
+    ('any crab','goron spice'): 'Crab Stir-Fry',
+    ('any seafood','raw meat'): 'Meat and Seafood Fry',
+    ('any seafood','raw meat'): 'Meat and Seafood Fry',
+    ('any seafood','prime raw meat'): 'Prime Meat and Seafood Fry',
+    ('any seafood', 'gourmet raw meat'): 'Gourmet Meat and Seafood Fry',
+    ('any plant', 'rock salt'): 'Salt-Grilled Greens',
+    ('any mushrooms', 'rock salt'): 'Salt-Grilled Mushrooms',
+    ('raw meat', 'rock salt'): 'Salt-Grilled Meat',
+    ('raw prime meat', 'rock salt'): 'Salt-Grilled Prime Meat',
+    ('raw gourmet meat', 'rock salt'): 'Salt-Grilled Prime Meat',
+    ('any fish', 'rock salt'): 'Salt-Grilled Fish',
+    ('any crab', 'rock salt'): 'Salt-Grilled Crab',
+    ('any plant', 'goron spice'): 'Herb Saute',
+    ('any mushroom','goron spice'): 'Fragrant Mushroom Saute',
+    ('any meat', 'spicy pepper'): 'Pepper Steak',
+    ('any meat', 'spicy pepper'): 'Pepper Seafood',
+    ('any fish', 'oil jar', 'stambulb'): 'Fragrant Seafood Stew',
+    ('raw bird','drumstick', 'oil jar'): 'Deep-Fried Drumstick',
+    ('raw bird thigh', 'oil jar'): 'Deep-Fried Thigh',
+    ('raw whole bird', 'oil jar'): 'Deep-Fried Bird Roast',
+    ('any seafood', 'tabantha wheat','goat butter'): 'Seafood Meuniere',
+    ('any porgy', 'tabantha wheat','goat butter'): 'Porgy Meuniere',
+    ('any salmon', 'tabantha wheat','goat butter'): 'Salmon Meuniere',
+    ('any fruit', 'any mushroom'): 'Fruit and Mushroom Mix',
+    ('any fruit'): 'Simmered Fruit',
+    ('hylian tomato'): 'Simmered Tomato',
+    ('any fruit'): 'Simmered Fruit',
+    ('hylian tomato','fresh milk', 'rock salt'): 'Fruity Tomato Stew',
+    ('any mushrooms','hylian tomato'): 'Tomato Mushroom Stew',
+    ('any seafood','hylian tomato'): 'Tomato Seafood Stew',
+    ('any plant','rock salt','fresh milk'): 'Cream of Vegetable Soup',
+    ('any vegetable','rock salt','fresh milk'): 'Veggie Cream Soup',
+    ('any mushroom','rock salt','fresh milk'): 'Cream of Mushroom Soup',
+    ('any meat', 'any plant','rock salt','fresh milk'): 'Creamy Meat Soup',
+    ('any seafood', 'any plant','rock salt','fresh milk'): 'Creamy Seafood Soup',
+    ('hearty blueshell snail','goat butter', 'tabantha wheat','fresh milk'): 'Snail Chowder',
+    ('radish','hydromelon','voltfruit','fresh milk'): 'Creamy Heart Soup',
+    ('any plant','courser bee honey'): 'Glazed Veggies',
+    ('any mushrooms', 'courser bee honey'): 'Glazed Mushrooms',
+    ('raw meat', 'courser bee honey'): 'Glazed Meat',
+    ('hylian rice', 'goron spice'): 'Curry Rice',
+    ('hylian rice', 'goron spice', 'any vegetable'): 'Vegetable Curry',
+    ('hylian rice', 'goron spice', 'raw meat'): 'Meat Curry',
+    ('hylian rice', 'goron spice', 'raw prime meat'): 'Prime Meat Curry',
+    ('hylian rice', 'goron spice', 'raw gourmet meat'): 'Gourmet Meat Curry',
+    ('hylian rice', 'goron spice', 'raw bird drumstick'): 'Poultry Curry',
+    ('hylian rice', 'goron spice', 'raw bird thigh'): 'Prime Poultry Curry',
+    ('hylian rice', 'goron spice', 'raw whole bird'): 'Gourmet Poultry Curry',
+    ('hylian rice', 'goron spice', 'any porgy'): 'Seafood Curry',
+    ('hylian rice', 'goron spice', 'hateno cheese'): 'Cheesy Curry',
+    ('any carrot', 'goat butter', 'fresh milk', 'tabantha wheat'): 'Carrot Stew',
+    ('fortified pumpkin', 'goat butter', 'fresh milk', 'tabantha wheat'): 'Pumpkin Stew',
+    ('any meat', 'goat butter', 'fresh milk', 'tabantha wheat'): 'Meat Stew',
+    ('prime meat', 'goat butter', 'fresh milk', 'tabantha wheat'): 'Prime Meat Stew',
+    ('gourmet meat', 'goat butter', 'fresh milk', 'tabantha wheat'): 'Gourmet Meat Stew',
+    ('fortified pumpkin', 'any meat'): 'Fortified Meat-Stuffed Pumpkin',
+    ('any vegetable', 'hylian rice', 'goat butter', 'rock salt'): 'Vegetable Risotto',
+    ('any mushroom', 'hylian rice', 'goat butter', 'rock salt'): 'Mushroom Risotto',
+    ('any salmon', 'hylian rice', 'goat butter', 'rock salt'): 'Salmon Risotto',
+    ('any crab', 'hylian rice', 'goat butter', 'rock salt'): 'Crab Risotto',
+    ('any mushrooms', 'hylian rice', 'goat butter', 'hateno cheese', 'rock salt'): 'Cheesy Risotto',
+    ('raw bird drumstick', 'hylian rice', 'goat butter', 'bird egg'): 'Poultry Pilaf',
+    ('raw bird thigh', 'hylian rice', 'goat butter', 'bird egg'): 'Prime Poultry Pilaf',
+    ('raw whole bird', 'hylian rice', 'goat butter', 'bird egg'): 'Gourmet Poultry Pilaf',
+    ('mighty porgy', 'iron shell crab', 'hylian rice', 'goat butter', 'rock salt'): 'Seafood Paella',
+    ('hylian rice', 'goat butter', 'goron spice'): 'Curry Pilaf',
+    ('hylian rice', 'bird egg'): 'Fried Egg and Rice',
+    ('any meat', 'hylian rice', 'oil jar', 'bird egg'): 'Crunchy Fried Rice',
+    ('any porgy', 'hylian rice', 'rock salt'): 'Seafood Fried Rice',
+    ('any crab', 'bird egg', 'hylian rice', 'rock salt'): 'Crab Omelet with Rice',
+    ('raw meat', 'hylian rice', 'rock salt'): 'Meat and Rice Bowl',
+    ('raw prime meat', 'hylian rice', 'rock salt'): 'Prime Meat and Rice Bowl',
+    ('raw gourmet meat', 'hylian rice', 'rock salt'): 'Gourmet Meat and Rice Bowl',
+    ('raw meat', 'hateno cheese' 'hylian rice', 'rock salt'): 'Cheesy Meat Bowl',
+    ('raw prime meat', 'hateno cheese' 'hylian rice', 'rock salt'): 'Prime Cheesy Meat Bowl',
+    ('raw gourmet meat', 'hateno cheese', 'hylian rice', 'rock salt'): 'Gourmet Cheesy Meat Bowl',
+    ('any plant', 'hylian rice'): 'Veggie Rice Balls',
+    ('any mushrooms', 'hylian rice'): 'Mushroom Rice Balls',
+    ('any meat', 'hylian rice'): 'Meaty Rice Balls',
+    ('any seafood', 'hylian rice'): 'Seafood Rice Balls',
+    ('any plant', 'bird egg' 'hylian rice', 'fresh milk'): 'Veggie Porridge',
+    ('tabantha wheat', 'rock salt'): 'Wheat Bread',
+    ('tabantha wheat', 'rock salt', 'hateno cheese'): 'Melty Cheesy Bread',
+    ('tabantha wheat', 'rock salt', 'hylian tomato', 'hateno cheese'): 'Hylian Tomato Pizza',
+    ('any meat', 'goat butter', 'tabantha wheat', 'rock salt'): 'Meat Pie',
+    ('any seafood', 'goat butter', 'tabantha wheat', 'rock salt'): 'Fish Pie',
+    ('hylian tomato', 'hateno cheese'): 'Cheesy Tomato',
+    ('any seafood', 'hateno cheese'): 'Cheesy Baked Fish', 
+    ('bird egg'): 'Omelet',
+    ('any plant', 'bird egg' 'goat butter', 'rock salt'): 'Vegetable Omelet',
+    ('any mushrooms', 'bird egg', 'goat butter', 'rock salt'): 'Mushroom Omelet',
+    ('any mushrooms', 'bird egg', 'goat butter', 'rock salt', 'hateno cheese'): 'Cheesy Omelet',
+    ('apple', 'goat butter'): 'Hot Buttered Apple',
+    ('any fruits', 'courser bee honey'): 'Honeyed Fruits',
+    ('courser bee honey'): 'Energising Honey Candy',
+    ('mighty banana', 'tabantha wheat', 'cane sugar'): 'Fried Bananas',
+    ('bird egg', 'fresh milk', 'cane sugar'): 'Egg Pudding',
+    ('tabantha wheat', 'bird egg', 'fresh milk', 'cane sugar'): 'Plain Crepe',
+    ('wildberry', 'tabantha wheat', 'bird egg', 'fresh milk', 'cane sugar'): 'Wildberry Crepe',
+    ('courser bee honey', 'tabantha wheat', 'bird egg', 'fresh milk', 'cane sugar'): 'Honey Crepe',
+    ('any nut', 'tabantha wheat', 'cane sugar', 'goat butter'): 'Nutcake',
+    ('any fruit', 'tabantha wheat', 'cane sugar'): 'Fruitcake',
+    ('any carrot', 'tabantha wheat', 'cane sugar'): 'Carrot Cake',
+    ('fortified pumpkin', 'goat butter' 'tabantha wheat', 'cane sugar'): 'Pumpkin Pie',
+    ('hateno cheese', 'goat butter', 'tabantha wheat', 'cane sugar'): 'Fruitcake',
+    ('any fruit', 'goat butter' 'tabantha wheat', 'cane sugar'): 'Fruit Pie',
+    ('apple', 'goat butter' 'tabantha wheat', 'cane sugar'): 'Apple Pie',
+    ('bird egg', 'goat butter' 'tabantha wheat', 'cane sugar'): 'Egg Tart',
+    ('fresh milk'): 'Warm Milk',
+    ('palm fruit', 'hydromelon', 'voltfruit', 'rock salt'): 'Noble Pursuit',
+    ('monster extract', 'any meat', 'any seafood'): 'Monster Stew',
+    ('monster extract', 'goat butter' 'fresh milk', 'tabantha wheat'): 'Monster Stew',
+    ('monster extract', 'hylian rice', 'goron spice'): 'Monster Curry',
+    ('monster extract', 'hylian rice', 'rock salt'): 'Monster Rice Balls',
+    ('monster extract', 'cane sugar', 'goat butter', 'tabantha wheat'): 'Monster Cake',
+    ('dark clump', 'any meat', 'any seafood'): 'Dark Stew',
+    ('dark clump', 'goat butter', 'fresh milk', 'tabantha wheat'): 'Dark Stew',
+    ('dark clump', 'hylian rice', 'goron spice'): 'Dark Curry',
+    ('dark clump', 'hylian rice', 'rock salt'): 'Dark Rice Ball',
+    ('dark clump', 'cane sugar', 'goat butter', 'tabantha wheat'): 'Dark Cake'
+    }
+
+
+def combine_food(items):
+    items=sorted(items)
+    combination = tuple(items)
+    return food_combinations.get (combination, 'Ew, you’ve made Dubious Food! Not sure if you should eat that :( ')
+
+def main():
+    print('Welcome to the Zelda Food Combiner!')
+    print('Enter two or more food items (separated by commas)to see what you get!')
+
+    while True:
+        user_input = input("type 'quit' to exit: ").strip().split(',')
+        
+        if user_input[0].strip().lower() == 'quit':
+            print("Goodbye!")
+            break
+            
+        if len(user_input) < 2 or len(user_input) > 6:
+            print('Please enter between 2 and 6 items.')
+            continue
+            
+        result = combine_food(user_input)
+        print('Result:', result)
+
+if __name__ == "__main__":
+    main()
